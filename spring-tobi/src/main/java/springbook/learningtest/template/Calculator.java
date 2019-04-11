0c662524e148573ebfe9f80d80eea222ede13544
dasop <orangedasop@naver.com>
2019-04-11 10:20:06 +0900

    템플릿/콜백

--- spring-tobi/src/main/java/springbook/learningtest/template/Calculator.java
+++ spring-tobi/src/main/java/springbook/learningtest/template/Calculator.java
@@ -5,9 +5,26 @@ import java.io.FileReader;
 import java.io.IOException;
 
 public class Calculator {
+	
 	public Integer calcSum(String filepath) throws IOException{
-		BufferedReader br = null;
-		try {
+		
+		LineCallback sumCallback=
+				new LineCallback() {
+			public Integer doSomethingWithLine(String line, Integer value) {
+				return value+Integer.valueOf(line);
+			}};
+			return lineReadTemplate(filepath,sumCallback,0);
+
+		/*
+		 * BufferedReaderCallback sumCallback = new BufferedReaderCallback() { public
+		 * Integer doSomethingWithReader(BufferedReader br) throws IOException{ Integer
+		 * sum = 0; String line = null; while((line = br.readLine())!= null) { sum +=
+		 * Integer.valueOf(line); } return sum; } };
+		 * 
+		 * return fileReadTemplate(filepath,sumCallback);
+		 */
+//	BufferedReader br = null;		
+		/*try {
 			br = new BufferedReader(new FileReader(filepath));
 			Integer sum = 0;
 			String line = null;
@@ -20,6 +37,54 @@ public class Calculator {
 			System.out.println(e.getMessage());
 			throw e;
 		}
+		finally {
+			if(br != null) { try {br.close();} catch(IOException e) {System.out.println(e.getMessage());}}
+		}*/
+	}
+	
+	public Integer calcMultiply(String filepath) throws IOException{
+		LineCallback multiplyCallback =
+				new LineCallback() {
+			public Integer doSomethingWithLine(String line, Integer value) {
+				return value * Integer.valueOf(line);
+			}};
+			return lineReadTemplate(filepath,multiplyCallback,1);
+		/*
+		 * BufferedReaderCallback multiplyCallback= new BufferedReaderCallback() {
+		 * public Integer doSomethingWithReader(BufferedReader br) throws IOException {
+		 * Integer multiply = 1; String line = null; while((line=br.readLine()) !=
+		 * null){ multiply *= Integer.valueOf(line); } return multiply; } }; return
+		 * fileReadTemplate(filepath,multiplyCallback);
+		 */
+	}
+	
+	public Integer fileReadTemplate(String filepath, BufferedReaderCallback callback) throws IOException{
+		BufferedReader br = null;
+		try {
+			br = new BufferedReader(new FileReader(filepath));
+			int ret = callback.doSomethingWithReader(br);
+			return ret;
+		}
+		catch(IOException e){
+			System.out.println(e.getMessage()); throw e;}
+		finally {
+			if(br != null) { try {br.close();} catch(IOException e) {System.out.println(e.getMessage());}}
+		}
+	} 
+	
+	public Integer lineReadTemplate(String filepath, LineCallback callback, int initVal) throws IOException{
+		BufferedReader br = null;
+		try {
+			br = new BufferedReader(new FileReader(filepath));
+			Integer res = initVal;
+			String line = null;
+			while((line=br.readLine())!=null) {
+				res=callback.doSomethingWithLine(line, res);
+			}
+			return res;
+		}
+		catch(IOException e){
+			System.out.println(e.getMessage()); throw e;}
 		finally {
 			if(br != null) { try {br.close();} catch(IOException e) {System.out.println(e.getMessage());}}
 		}
