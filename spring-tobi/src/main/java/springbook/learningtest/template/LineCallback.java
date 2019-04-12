a5235b436a9cdc817414ef9035f497b33cd578fb
dasop <orangedasop@naver.com>
2019-04-12 11:02:45 +0900

    P257 제네릭스 이용한 콜백

--- spring-tobi/src/main/java/springbook/learningtest/template/LineCallback.java
+++ spring-tobi/src/main/java/springbook/learningtest/template/LineCallback.java
@@ -1,6 +1,6 @@
 package springbook.learningtest.template;
 
-public interface LineCallback {
-	Integer doSomethingWithLine(String line, Integer value);
+public interface LineCallback<T> {
+	T doSomethingWithLine(String line, T value);
 
 }
